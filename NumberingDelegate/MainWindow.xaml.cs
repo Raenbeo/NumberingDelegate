b523582495c1cfc608e978f4f862f0e7527ce34a
Raenbeo <akwdkk39@gmail.com>
2022-12-09 14:26:53 +0900

    second-commit

--- NumberingDelegate/MainWindow.xaml.cs
+++ NumberingDelegate/MainWindow.xaml.cs
@@ -30,12 +30,24 @@ namespace NumberingDelegate
         private void OkBtn_Click(object sender, RoutedEventArgs e)
         {
             bool canPass = true;
-             if(string.IsNullOrEmpty(NowCommonPathTxt.Text) == true) canPass = false;
-             if(string.IsNullOrEmpty(CoummonExtensionTxt.Text) == true) canPass = false;
+            if (string.IsNullOrEmpty(NowCommonPathTxt.Text) == true) canPass = false;
+            if (string.IsNullOrEmpty(CoummonExtensionTxt.Text) == true) canPass = false;
+            if (string.IsNullOrEmpty(StartNumberTxt.Text) == true) canPass = false;
 
-            if (canPass == false) 
+            if (canPass == false)
             {
                 System.Windows.Forms.MessageBox.Show("빈칸없이 채워주세요 ^^");
+                return;
+            }
+
+            uint count = 0;
+            try
+            {
+                count = Convert.ToUInt32(StartNumberTxt.Text);
+            }
+            catch
+            {
+                System.Windows.Forms.MessageBox.Show("시작번호는 숫자만 입력할 수 있습니다.");
                 return;
             }
 
@@ -43,15 +55,30 @@ namespace NumberingDelegate
             {
                 if (NumberDGrid.Items.Count >= 1)
                 {
-                    //string newName = NowCommonPathTxt.Text + CommonNameTxt.Text+ + "." + CoummonExtensionTxt.Text;
+                    string folderPath = NowCommonPathTxt.Text + "\\" + "Numbering";
+                    DirectoryInfo di = new DirectoryInfo(folderPath);
+
+                    if (di.Exists == false)
+                    {
+                        di.Create();
+                    }
+
 
                     StringBuilder stringBuilder = new StringBuilder();
                     stringBuilder.Clear();
 
-                    int count = 1;
-                    foreach(NumberingItem ain in NumberDGrid.Items)
+                    foreach (NumberingItem ain in NumberDGrid.Items)
                     {
-                        stringBuilder.Append(NowCommonPathTxt.Text).Append("\\").Append(CommonNameTxt.Text).Append(count.ToString()).Append(".").Append(CoummonExtensionTxt.Text);
+                        stringBuilder.Append(folderPath)
+                            .Append("\\")
+                            .Append(CommonNameTxt.Text)
+                            .Append(count.ToString());
+
+                        if (CoummonExtensionTxt.Text.Contains(".") == false)
+                            stringBuilder.Append(".");
+
+                            stringBuilder.Append(CoummonExtensionTxt.Text);
+
                         System.Diagnostics.Debug.WriteLine(stringBuilder.ToString());
                         ain.Rename(stringBuilder.ToString());
                         stringBuilder.Clear();
@@ -60,10 +87,20 @@ namespace NumberingDelegate
 
                     System.Windows.MessageBox.Show("넘버링 완료");
 
-                    int itemCount = NumberDGrid.Items.Count;
-                    for (int i = 0; i < itemCount; i++) NumberDGrid.Items.RemoveAt(0);
+                    uint itemCount = (uint)NumberDGrid.Items.Count;
+                    for (uint i = 0; i < itemCount; i++) NumberDGrid.Items.RemoveAt(0);
 
                     NumberDGrid.Items.Refresh();
+
+                    if (AddAllStartChx.IsChecked == true)
+                    {
+                        uint all_count = Convert.ToUInt32(AllCountTxt.Text);
+                        uint start_number = Convert.ToUInt32(StartNumberTxt.Text);
+
+                        uint new_start_number = all_count + start_number;
+
+                        StartNumberTxt.Text = new_start_number.ToString();
+                    }
                 }
             }
             else
@@ -83,23 +120,22 @@ namespace NumberingDelegate
                 string[] Names = fileDialog.SafeFileNames;
                 string[] AllPathes = fileDialog.FileNames;
 
-                for (int i = 0; i < Names.Length; i++)
+                for (uint i = 0; i < Names.Length; i++)
                 {
-                    NumberDGrid.Items.Add(new NumberingItem(Names[i],AllPathes[i]));
+                    NumberDGrid.Items.Add(new NumberingItem(Names[i], AllPathes[i]));
                 }
 
                 NumberDGrid.Items.Refresh();
 
-                string removeName = "\\" + Names[0]; 
+                string removeName = "\\" + Names[0];
 
-                string path = AllPathes[0].Replace(removeName,string.Empty);
+                string path = AllPathes[0].Replace(removeName, string.Empty);
 
                 NowCommonPathTxt.Text = path;
                 AllCountTxt.Text = NumberDGrid.Items.Count.ToString();
             }
         }
     }
-
 }
 
 public class NumberingItem
